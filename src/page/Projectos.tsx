5724f0eceffd562a659c9c6ddfb003936e668767
yoan2024 <yhoangaga2025@gmail.com>
2025-07-11 13:59:44 -0600

    solucionanado error de dominio

--- src/page/Projectos.tsx
+++ src/page/Projectos.tsx
@@ -1,4 +1,31 @@
+import projects from "../P.json";
+import TarjetaProjecto from "../components/TarjetProjecto";
+
 const Projectos = () => {
-  return <div className="text-white w-full h-full px-5">jnkjnkjnkjn</div>;
+  return (
+    <div className="text-white w-full h-full px-5">
+      <div>
+        <div className="flex flex-col gap-3 ">
+          <div className=" font-bold text-yellow-400 text-2xl">
+            {/*mi nombre*/}
+            Projectos
+          </div>
+          <div className=" flex flex-row  flex-wrap gap-4 max-w-3xl">
+            {projects.map((p, index) => {
+              return (
+                <TarjetaProjecto
+                  key={index}
+                  title={p.title}
+                  description={p.description}
+                  techs={p.techs}
+                  link={p.link}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Projectos;
